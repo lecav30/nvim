30060115c4ab677221d474e31e6f3616b9797316
lecav <lecav30@outlook.com>
2022-04-29 18:14:01 -0500

    Neovim Files

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- aliases
+local fn = vim.fn
+local cmd = vim.cmd
+
+-- Set leader key to space
+vim.g.mapleader = ' '
+
+require('settings')
+require('keymappings')
+require('plugins')
+require('plugins_settings')
+
+-- Mapping helper
+-- local mapper = function(mode, key, result)
+--   vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
+-- end
+
