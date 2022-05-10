2fce45a16b8d411e6099e110dd577c53da35a7b8
lecav <lecav30@outlook.com>
2022-05-10 13:29:48 -0500

    Config tree

--- lua/plugins_settings/tree.lua
+++ lua/plugins_settings/tree.lua
@@ -67,8 +67,8 @@ require'nvim-tree'.setup {
     indent_markers = {
       enable = false,
       icons = {
-        corner = "L ",
-        edge = "|│ ",
+        corner = "└ ",
+        edge = "| ",
         none = "  ",
       },
     },
@@ -119,7 +119,7 @@ require'nvim-tree'.setup {
       restrict_above_cwd = false,
     },
     open_file = {
-      quit_on_open = false,
+      quit_on_open = true,
       resize_window = false,
       window_picker = {
         enable = true,
@@ -154,3 +154,4 @@ local mapper = function(mode, key, result)
 end
 
 mapper("n", "<Leader>e", ":NvimTreeToggle<CR>")
+
