b686113f9d455cfc576acf667152d669924b347a
lecav <lecav30@outlook.com>
2022-05-10 22:13:53 -0500

    Modify plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,9 +44,11 @@ return require('packer').startup(function()
 
   -- Tree file explorer
   use {
-      'kyazdani42/nvim-tree.lua',
-      requires = 'kyazdani42/nvim-web-devicons',
-      -- config = function() require'nvim-tree'.setup {} end
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+    },
+    tag = 'nightly'
   }
 
   -- Completion engine plugin 
