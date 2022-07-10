f078217ea87118147554a64b0c8835e62e23d270
lecav <lecav30@outlook.com>
2022-07-10 17:32:59 -0500

    Disable format in save

--- lua/plugins_settings/lsp.lua
+++ lua/plugins_settings/lsp.lua
@@ -20,13 +20,13 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 
-    -- Format on save
-    if client.resolved_capabilities.document_formatting then
-      vim.api.nvim_command [[augroup Format]]
-      vim.api.nvim_command [[autocmd! * <buffer>]]
-      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-      vim.api.nvim_command [[augroup END]]
-    end
+  -- Format on save
+    -- if client.resolved_capabilities.document_formatting then
+    --   vim.api.nvim_command [[augroup Format]]
+    --   vim.api.nvim_command [[autocmd! * <buffer>]]
+    --   vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    --   vim.api.nvim_command [[augroup END]]
+    -- end
 end
 
 
