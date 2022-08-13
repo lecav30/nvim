66a8e54dc896c97052e7ff98f1bf151dc126a240
lecav30 <lecav30@outlook.com>
2022-08-13 18:07:36 -0500

    Indent 4 spaces

--- lua/plugins_settings/neoformat.lua
+++ lua/plugins_settings/neoformat.lua
@@ -3,3 +3,7 @@
 -- custom setting for clangformat
 vim.g.neoformat_enabled_cpp = clangformat
 vim.g.neoformat_enabled_c = clangformat
+vim.g.neoformat_cpp_clangformat = {
+  exe = "clang-format",
+  args = {"--style=\"{BasedOnStyle: Google, IndentWidth: 4}\"",},
+}
