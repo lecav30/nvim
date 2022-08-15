fde7cbcb31455df79ef886d30cdda277a777b5cb
lecav30 <lecav30@outlook.com>
2022-08-15 18:31:49 -0500

    Add key to format

--- lua/plugins_settings/neoformat.lua
+++ lua/plugins_settings/neoformat.lua
@@ -9,3 +9,9 @@ vim.g.neoformat_cpp_clangformat = {
 }
 
 vim.g.neoformat_enabled_lua = luafmt
+
+local mapper = function(mode, key, result)
+    vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
+end
+
+mapper("n", "<Leader>fn", ":Neoformat<CR>")
