69ef8b1c9503bb363d886f4015f051824b809b1d
lecav30 <lecav30@outlook.com>
2022-09-08 13:12:28 -0500

    No wrap

--- lua/settings.lua
+++ lua/settings.lua
@@ -43,22 +43,21 @@ o.foldmethod = "manual" -- indentation folds
 o.relativenumber = true
 o.number = true
 o.signcolumn = "yes" -- make sure this is on for gitsigns.nvim, otherwise the signcolumn changes size constantly
-o.wrap = true -- text autofit in a line
+o.wrap = false -- text autofit in a line
 
 -- colorscheme global defaults
-o.background = "dark"
+-- o.background = "dark"
 o.termguicolors = true
 
 -- Themes and configs
 -- cmd('colorscheme material')
--- vim.cmd [[colorscheme moonfly]]
--- vim.cmd [[colorscheme rvcs]]
+vim.cmd [[colorscheme moonfly]]
 -- vim.g.neon_style = "default"
 -- vim.g.neon_italic_keyword = true
 -- vim.g.neon_italic_function = true
 -- vim.g.neon_transparent = true
 -- vim.cmd[[colorscheme neon]]
-vim.cmd [[colorscheme nightfly]]
+-- vim.cmd [[colorscheme nightfly]]
 
 -- Set completeopt to have a better completion experience
 o.completeopt = "menuone,noselect"
