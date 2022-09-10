171cd1b462774d10cfcbaaedcf3892144e0dd7f4
lecav30 <lecav30@outlook.com>
2022-09-10 17:14:23 -0500

    No italics

--- lua/plugins_settings/themer.lua
+++ lua/plugins_settings/themer.lua
@@ -2,11 +2,11 @@ require("themer").setup(
     {
         colorscheme = "catppuccin",
         styles = {
-            ["function"] = {style = "italic"},
-            functionbuiltin = {style = "italic"},
-            variable = {style = "italic"},
-            variableBuiltIn = {style = "italic"},
-            parameter = {style = "italic"}
+            -- ["function"] = {style = "italic"},
+            -- functionbuiltin = {style = "italic"},
+            -- variable = {style = "italic"},
+            -- variableBuiltIn = {style = "italic"},
+            -- parameter = {style = "italic"}
         }
     }
 )
