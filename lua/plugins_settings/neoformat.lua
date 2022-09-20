bb42b23ca1bfa699019f6dca6426778201beb003
lecav30 <lecav30@outlook.com>
2022-09-20 17:28:08 -0500

    Add prettier

--- lua/plugins_settings/neoformat.lua
+++ lua/plugins_settings/neoformat.lua
@@ -1,14 +1,22 @@
 -- Reference:
 -- https://jdhao.github.io/2020/04/19/nvim_cpp_and_c_completion/
 -- custom setting for clangformat
-vim.g.neoformat_enabled_cpp = clangformat
-vim.g.neoformat_enabled_c = clangformat
-vim.g.neoformat_cpp_clangformat = {
+local g = vim.g
+
+g.neoformat_enabled_cpp = { "clangformat" }
+g.neoformat_cpp_clangformat = {
     exe = "clang-format",
     args = {'--style="{BasedOnStyle: Google, IndentWidth: 4}"'}
 }
-
-  vim.g.neoformat_enabled_lua = luafmt
+g.neoformat_enabled_c = { "clangformat" }
+g.neoformat_enabled_lua = { "luafmt" }
+g.neoformat_enabled_python = {"black", "isort"}
+g.neoformat_enabled_yaml = {"prettier"}
+g.neoformat_enabled_html = {"prettier"}
+g.neoformat_enabled_css = {"prettier"}
+g.neoformat_enabled_javascript = {"prettier"}
+g.neoformat_enabled_typescript = {"prettier"}
+g.neoformat_enabled_markdown = {"prettier"}
 
 local mapper = function(mode, key, result)
     vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
