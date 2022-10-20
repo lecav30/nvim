20358db3e29945e770870a6114cd52d65c32d93d
lecav30 <lecav30@outlook.com>
2022-10-20 13:46:21 -0500

    Update lsp

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -37,10 +37,13 @@ local on_attach = function(client, bufnr)
 end
 
 -- local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 local servers = {"tsserver", "clangd", "pyright", "html", "cssls", "sumneko_lua"}
 
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
-        on_attach = on_attach
+        on_attach = on_attach,
+        capabilities = capabilities
     }
 end
