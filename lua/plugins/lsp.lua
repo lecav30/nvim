857801a0189504d21b62c7dfda70e1ec05ce71d7
Eduardo Sebastián Lévano Cavero <79949221+lecav30@users.noreply.github.com>
2025-08-21 18:13:11 -0500

    feat: snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,6 +12,18 @@ return {
 	"L3MON4D3/LuaSnip", -- Snippets
 	"saadparwaiz1/cmp_luasnip", -- Snippets with cmp
 	"rafamadriz/friendly-snippets", -- Friendly snippets
+	{
+		"mlaursen/vim-react-snippets",
+		dependencies = { "L3MON4D3/LuaSnip" },
+		config = function()
+			require("vim-react-snippets").lazy_load()
+
+			local config = require("vim-react-snippets.config")
+			-- config.readonly_props = false -- Si no quieres envolver props como Readonly<T>
+			-- config.test_framework = "vitest" -- Para usar Vitest en tus tests
+			-- config.test_renderer_path = "@/test-utils" -- Para personalizar el path del renderer
+		end,
+	}, -- React snippets
 	---------------------------------------------------------------------------------
 	---------------------------- Better performance LSP -----------------------------
 	---------------------------------------------------------------------------------
@@ -22,8 +34,8 @@ return {
 	---------------------------------------------------------------------------------
 	"williamboman/mason.nvim",
 	-- "williamboman/mason-lspconfig.nvim", -- Install LSP servers and provide configurations
-  -- It's not strictly necessary, but it's usefull if you want to use the LSP servers
-  -- that are installed by Mason without worrying about the configurations.
+	-- It's not strictly necessary, but it's usefull if you want to use the LSP servers
+	-- that are installed by Mason without worrying about the configurations.
 	---------------------------------------------------------------------------------
 	----------------------------------- Utility -------------------------------------
 	---------------------------------------------------------------------------------
