cdda815fe107a03eeaf1816e1bf79ed6c5593255
lecav30 <lecav30@outlook.com>
2025-09-23 10:49:41 -0500

    feat: gitsigns conf

--- lua/plugins_settings/gitsigns.lua
+++ lua/plugins_settings/gitsigns.lua
@@ -7,7 +7,7 @@ require("gitsigns").setup({
 		changedelete = { text = "~" },
 		untracked = { text = "┆" },
 	},
-	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	signcolumn = true,
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
@@ -16,7 +16,7 @@ require("gitsigns").setup({
 		follow_files = true,
 	},
 	attach_to_untracked = true,
-	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame = true,
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
