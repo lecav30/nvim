657b8c6adb3a9625f1d8a9c25c9623c2a274aa02
lecav30 <lecav30@outlook.com>
2025-09-26 11:26:48 -0500

    fix: findfiles

--- lua/plugins_settings/telescope.lua
+++ lua/plugins_settings/telescope.lua
@@ -12,22 +12,6 @@ local ignore_these = {
 	"package%-lock.json",
 }
 
-local webdev_dash_keywords = {
-	"css",
-	"cssmedia",
-	"html",
-	"htmle",
-	"htmlhead",
-	"httpheaders",
-	"httpstatus",
-	"js",
-	"nextjs",
-	"nodejs",
-	"react",
-	"scss",
-	"ts",
-}
-
 local actions = require("telescope.actions")
 local fb_actions = require("telescope").extensions.file_browser.actions
 local function telescope_buffer_dir()
@@ -58,6 +42,15 @@ require("telescope").setup({
 			},
 			layout_strategy = "vertical",
 			theme = "dropdown",
+      -- Temporal fix for scaped characters in windows
+			find_command = {
+				"rg",
+				"--files",
+				"--glob",
+				"!{.git/*,.svelte-kit/*,target/*,node_modules/*}",
+				"--path-separator",
+				"/",
+			},
 		},
 		live_grep = {
 			layout_config = {
@@ -119,30 +112,6 @@ require("telescope").load_extension("file_browser")
 require("telescope").load_extension("find_pickers")
 require("telescope").load_extension("notify")
 -- require("telescope").load_extension("dap")
-
--- local builtin = function(mapping, picker, is_custom)
--- 	local module = is_custom and "plugins_settings.telescope" or "telescope.builtin"
--- 	local rhs = string.format([[<cmd>lua require'%s'.%s()<cr>]], module, picker)
--- 	vim.api.nvim_set_keymap("n", mapping, rhs, { noremap = true, silent = true })
--- end
-
--- builtin("<leader>ff", "find_files") -- Lists files in your current working directory, respects .gitignore
--- builtin("<leader>of", "oldfiles") -- Lists previously open files
--- Search word
--- builtin("<leader>fw", "current_buffer_fuzzy_find") -- Live fuzzy search inside of the currently open buffer
-
--- Git
--- Commits
--- builtin("<leader>gc", "git_commits")
--- Branches
--- builtin("<leader>gb", "git_branches")
--- Status
--- builtin("<leader>gs", "git_status")
-
--- Help
--- builtin("<leader>gh", "help_tags") -- Lists available help tags and opens a new window with the relevant help info on <cr>
--- Tabs (buffers)
--- builtin("<leader>bl", "buffers") -- Lists open buffers in current neovim instance
 
 -- Functions
 -- https://github.com/nvim-telescope/telescope.nvim#default-mappings
